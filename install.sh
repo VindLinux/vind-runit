89ca0fc14f1c699e88f7eb035e17e4f993eb943d
Elgumi01 <miguelbarbosa2409@gmail.com>
2026-08-30 17:24:05 -0300

    install: update installation paths

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+set -e
 
-mkdir -p /etc/runit/utils
+mkdir -p /etc/runit/utils /etc/runit/service
+
+cp -f runit/1 runit/2 runit/3 /etc/runit/
+chmod +x /etc/runit/1 /etc/runit/2 /etc/runit/3
+
 cp -f utils/* /etc/runit/utils/
 chmod +x /etc/runit/utils/*
+
+cp -f commands/* /usr/sbin/
+chmod +x /usr/sbin/poweroff /usr/sbin/reboot
 
 echo "vind-runit installed."

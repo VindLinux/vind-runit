ad825bbdb849d7f55e02fba1efb3bebb48ad062d
Elgumi01 <miguelbarbosa2409@gmail.com>
2026-08-30 21:31:17 -0300

    service: add agetty service

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-mkdir -p /etc/runit/utils /etc/runit/service
+mkdir -p /etc/runit/utils
+mkdir -p /etc/service
 
 cp -f runit/1 runit/2 runit/3 /etc/runit/
 chmod +x /etc/runit/1 /etc/runit/2 /etc/runit/3
@@ -11,5 +12,13 @@ chmod +x /etc/runit/utils/*
 
 cp -f commands/* /usr/sbin/
 chmod +x /usr/sbin/poweroff /usr/sbin/reboot
+
+for service in service/*; do
+    name="$(basename "$service")"
+
+    mkdir -p "/etc/service/$name"
+    cp -f "$service/run" "/etc/service/$name/run"
+    chmod +x "/etc/service/$name/run"
+done
 
 echo "vind-runit installed."
